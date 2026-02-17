c6eeb7965ad25c7715e11312d3d43b98f86d58c3
Maria Elena Miranda <ifp.mara@gmail.com>
2026-02-17 21:03:54 +0100

    Creación de componente aside-bar

---
+++ src/app/layout/shell/components/aside-bar/aside-bar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-aside-bar',
+  imports: [RouterLink],
+  templateUrl: './aside-bar.html',
+  styleUrl: './aside-bar.css',
+})
+export class AsideBar {
+
+}
---
+++ src/app/layout/shell/shell.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AsideBar } from "./components/aside-bar/aside-bar";
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-shell',
+  imports: [AsideBar, RouterOutlet],
+  templateUrl: './shell.html',
+  styleUrl: './shell.css',
+})
+export class Shell {
+
+}
